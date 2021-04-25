658dd00cd9acba0e9ec46a0a09d308ac56eb33d5
David Tyler <david.tyler@metaswitch.com>
2021-04-25 16:38:40 +0100

    Added interruptsEnabled flag to cpu state across whole architecture

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@ docker run --rm --name sui -d -p 8080:8080 sui
 
 RESULT=`curl -s --header "Content-Type: application/json" \
   --request POST \
-  --data '{"id":"abcd", "opcode":128,"state":{"a":0,"b":1,"c":0,"d":5,"e":15,"h":10,"l":20,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":1,"stackPointer":2,"cycles":0}}' \
+  --data '{"id":"abcd", "opcode":128,"state":{"a":0,"b":1,"c":0,"d":5,"e":15,"h":10,"l":20,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":1,"stackPointer":2,"cycles":0,"interruptsEnabled":true}}' \
   http://localhost:8080/api/v1/execute?operand1=1`
-EXPECTED='{"id":"abcd", "opcode":128,"state":{"a":255,"b":1,"c":0,"d":5,"e":15,"h":10,"l":20,"flags":{"sign":true,"zero":false,"auxCarry":false,"parity":true,"carry":true},"programCounter":1,"stackPointer":2,"cycles":7}}'
+EXPECTED='{"id":"abcd", "opcode":128,"state":{"a":255,"b":1,"c":0,"d":5,"e":15,"h":10,"l":20,"flags":{"sign":true,"zero":false,"auxCarry":false,"parity":true,"carry":true},"programCounter":1,"stackPointer":2,"cycles":7,"interruptsEnabled":true}}'
 
 docker kill sui
 
